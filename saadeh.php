c981073f8a469ae645c739370258ac3a95f71c65
abboushi123 <aboodabboushi4@gmail.com>
2024-06-03 02:58:31 +0300

    frontEnd

---
+++ saadeh.php
@@ -0,0 +1,13 @@
+<?php
+$dogname='dog';
+echo `  <div class="card">
+            <div class="img" ><img src="image/dog.png" alt=""></div>
+                  <div class="desc">${dogname}</div>
+            <div class="title">Dogs</div>
+            <div class="box">
+                <div class="price">Price 50$</div>
+
+                <button class ="btn">Buy Now</button>
+            </div>
+        </div>`;
+
